e34b4c2ca46c9a7868f7b574b2b1ad6164d2535b
Cassandra Lee <cassandratlee@gmail.com>
2024-10-31 18:02:18 -0230

    Set up pattern and project to take null values instead of undefined.

--- server/controllers/fabric.js
+++ server/controllers/fabric.js
@@ -26,30 +26,64 @@ const setUpConnection = () => {
 export const addFabric = (req, res) => {
   const userID = 1;
   const {
-    fabricImage,
-    fabricName,
-    length,
-    width,
-    fibreType,
-    otherFibreType,
-    wovenOrKnit,
-    fabricType,
-    solidOrPrint,
-    printType,
-    dominantColour,
-    otherColour,
-    purchaseDate,
-    purchasedFrom,
-    price,
-    prewashed,
-    notes,
+    fabricImage = null,
+    fabricName = null,
+    length = null,
+    width = null,
+    fibreType = null,
+    otherFibreType = null,
+    wovenOrKnit = null,
+    fabricType = null,
+    solidOrPrint = null,
+    printType = null,
+    dominantColour = null,
+    otherColour = null,
+    purchaseDate = null,
+    purchasedFrom = null,
+    price = null,
+    prewashed = null,
+    notes = null,
   } = req.body;
   const con = setUpConnection();
   console.log(req.body);
 
   let sql = `INSERT INTO fabric 
-  (userID, fabricImage, fabricName, length, width, fibreType, otherFibreType, wovenOrKnit, fabricType, solidOrPrint, printType, dominantColour, otherColour, purchaseDate, purchasedFrom, price, prewashed, notes) 
-  VALUES (${userID}, '${fabricImage}', '${fabricName}', '${length}', '${width}', '${fibreType}', '${otherFibreType}', '${wovenOrKnit}', '${fabricType}', '${solidOrPrint}', '${printType}', '${dominantColour}', '${otherColour}', '${purchaseDate}', '${purchasedFrom}', '${price}', '${prewashed}', '${notes}')`;
+  (userID, 
+  fabricImage, 
+  fabricName, 
+  length, 
+  width, 
+  fibreType, 
+  otherFibreType, 
+  wovenOrKnit, 
+  fabricType, 
+  solidOrPrint, 
+  printType, 
+  dominantColour, 
+  otherColour, 
+  purchaseDate, 
+  purchasedFrom, 
+  price, 
+  prewashed, 
+  notes) 
+  VALUES (${userID}, 
+  ${fabricImage ? `'${fabricImage}'` : "NULL"}, 
+  ${fabricName ? `'${fabricName}'` : "NULL"}, 
+  ${length ? `'${length}'` : "NULL"}, 
+  ${width ? `'${width}'` : "NULL"}, 
+  ${fibreType ? `'${fibreType}'` : "NULL"}, 
+  ${otherFibreType ? `'${otherFibreType}'` : "NULL"}, 
+  ${wovenOrKnit ? `'${wovenOrKnit}'` : "NULL"}, 
+  ${fabricType ? `'${fabricType}'` : "NULL"}, 
+  ${solidOrPrint ? `'${solidOrPrint}'` : "NULL"}, 
+  ${printType ? `'${printType}'` : "NULL"}, 
+  ${dominantColour ? `'${dominantColour}'` : "NULL"}, 
+  ${otherColour ? `'${otherColour}'` : "NULL"}, 
+  ${purchaseDate ? `'${purchaseDate}'` : "NULL"}, 
+  ${purchasedFrom ? `'${purchasedFrom}'` : "NULL"}, 
+  ${price ? `'${price}'` : "NULL"}, 
+  ${prewashed ? `'${prewashed}'` : "NULL"}, 
+  ${notes ? `'${notes}'` : "NULL"})`;
 
   con.query(sql, (err, rows) => {
     con.destroy();
