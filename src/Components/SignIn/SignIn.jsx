e7e3ecbb0348d8d072dcc6bdc18a126bc4f4e0a4
Cassandra Lee <cassandratlee@gmail.com>
2024-12-04 17:16:51 -0330

    Start sign in page

--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -2,10 +2,35 @@
 
 const SignIn = () => {
 
+const handleLogin = (e) => {
+    e.preventDefault();
+}
+
 return (
     <div>
-        Sign In Form
+        <div>
+            <h2>Sign In</h2>
+            <form onSubmit={handleLogin}>
+            <input  
+                type="email"
+                placeholder="Enter email"
+                required
+            />
+            <br />
+            <input
+                type="password"
+                placeholder="Enter password"
+                required
+            />
+            <br />
+            <button type="submit">Submit</button>
+            </form>
+        </div>
+        <div>
+            
+        </div>
     </div>
+  
 );
 
 };
