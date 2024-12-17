807233fde5bb8dceeb360840d77fd1c9d90c509f
Cassandra Lee <cassandratlee@gmail.com>
2024-12-17 16:32:16 -0330

    Add amplify configure to main

--- src/main.jsx
+++ src/main.jsx
@@ -23,6 +23,10 @@ import SignIn from './Routes/SignIn';
 import SignUp from './Routes/SignUp';
 import '../src/Styles/index.css';
 import { UserProvider } from "./Context/UserContext.jsx";
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
 
 const router = createBrowserRouter([
   {
